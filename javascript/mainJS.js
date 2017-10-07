c7c7c54bce97628d410617baf79de04219fb1ace
plurnee <patricium33@gmail.com>
2017-10-08 00:44:00 +0800

    Add files via upload

---
+++ javascript/mainJS.js
@@ -0,0 +1,12 @@
+
+function openTourPackages() {
+    window.open('Tour%20Packages.html');
+}
+
+function openTownTour() {
+    window.open('Coron%20Town%20Tour.html');
+}
+
+function openAttractions() {
+    window.open('Attractions.html');
+}
